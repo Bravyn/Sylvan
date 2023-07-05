a7f0138a8402969b661a98754820f7aa1368db78
Ian Bravyn <ianbravynsa@gmail.com>
2023-07-05 22:41:32 +0300

    .

---
+++ readme.md
@@ -0,0 +1,24 @@
+
+# NeuroDash
+
+## Objective:
+Help the main character, Neuron Nate, navigate through the brain and collect neurotransmitters while avoiding obstacles and challenges.
+
+### Gameplay:
+1. Characters: You play as Neuron Nate, a lively neuron with a mission to gather neurotransmitters and keep the brain running smoothly.
+
+2. Brain Maze: The game takes place in a colorful and dynamic brain maze, filled with pathways, synapses, and obstacles.
+
+3. Collect Neurotransmitters: Your goal is to collect as many neurotransmitter icons scattered throughout the maze. Each neurotransmitter collected earns you points and helps keep the brain's communication in check.
+
+4. Avoid Obstacles: Watch out for obstacles such as misfiring neurons, toxic chemicals, or blocked synapses. Colliding with these obstacles slows you down or deducts points.
+
+5. Power-Ups: Along the way, you can collect power-ups like "Speed Boost" to enhance Neuron Nate's agility or "Shield" to protect against obstacles for a limited time.
+
+6. Time Pressure: As you progress, the game speed increases, challenging your reflexes and decision-making abilities. Quick thinking and agile maneuvers are key to success.
+
+7. Leaderboards: Compete against other players globally or with friends on online leaderboards to see who can achieve the highest score and become the ultimate Neurotransmitter Dash champion.
+
+8. Educational Insights: Between game levels, provide fun facts about different neurotransmitters, their functions, and their impact on the brain. This adds an educational element to the game, making it both entertaining and informative.
+
+Remember, the main goal of the game is to have fun while learning about neurotransmitters and the brain's intricate communication system. So, put on your gaming hat, dodge those obstacles, and collect those neurotransmitters to become a master of Neurotransmitter Dash!
---
+++ sylvan.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+def what_is_a_neurotransmitter():
+    st.subheader(":blue[NEURO]*DASH*")
+    st.info("""A neurotransmitter is a **chemical messanger**
+    that transmits signals between neurons(nerve cells)
+    in the brain and throughtout the nervous system. """)
+    with st.expander("Read about Neuro Transmitters"):
+        st.info("""
+        Imagine your brain as a bustling city with millions of tiny messengers running around, carrying important information from one place to another. These messengers are called neurotransmitters, and they have a vital job in making sure that different parts of your brain and nervous system can communicate effectively.
+
+Neurotransmitters are like chemical signals that help neurons (nerve cells) talk to each other. They act as messengers, relaying information from one neuron to the next. It's like passing notes in a classroom, but instead of paper, they use special chemicals to send their messages.
+
+When a neuron wants to send a signal, it releases neurotransmitters into the small gaps between neurons called synapses. These neurotransmitters then travel across the synapse and attach themselves to receptors on the receiving neuron, kind of like a key fitting into a lock. Once attached, they deliver their message to the receiving neuron.
+
+The receiving neuron receives these messages and decides what to do next based on the information it received. It may continue passing the signal to other neurons, or it may trigger a response in the body, like moving a muscle or releasing hormones.
+
+Think of neurotransmitters as the communication superheroes of your brain. They help coordinate your thoughts, emotions, movements, and all the things that make you, well, you! Without neurotransmitters, the different parts of your brain wouldn't be able to work together and communicate effectively.
+
+So, next time you're amazed by your brain's incredible abilities, remember to give a shout-out to these little chemical messengers, the neurotransmitters, who make it all possible!
+        """)
+    st.caption("""These chemical substances 
+    play a crucial role in facilatating 
+    communication and information transfer between 
+    nerve cells.""")
+
+def how_do_they_work():
+    st.info("""When an electrical signal, called an
+            *action potential* reaches the end of 
+             of a neuron, it triggers the release of 
+              neurotransmitter from specialized structures called 
+               synaptic vesicles """)
+    
+what_is_a_neurotransmitter()
+how_do_they_work()
